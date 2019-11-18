d61b78ab8206d4b3385ce5df183e965f35213e3e
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-17 18:30:44 -0800

    Finish user authentication

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-
 const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -17,16 +17,14 @@ app.get("/", (req, res) => {
     res.send("Hello World!!");
 });
 
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-
-
-
-
-
 
 const port = process.env.PORT || 5000;
 
