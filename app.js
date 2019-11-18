4c855974bb742a044704759429be9ec807bb6c0e
Jaehyuk Lee <jhlumd@gmail.com>
2019-11-18 02:14:40 -0800

    Before heroku push, added heroku settings

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+const path = require("path");
+
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
@@ -16,6 +18,13 @@ mongoose
 app.get("/", (req, res) => {
     res.send("Hello World!!");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
